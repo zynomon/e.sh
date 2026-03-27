dddb215e70bc377b021f3c99ea54c9188434c09d
Zynomon aelius <92979020+zynomon@users.noreply.github.com>
2026-03-27 15:39:37 +0600

    Update and rename NS26.sh to LNS26.sh

--- LNS26.sh
+++ LNS26.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 echo "run this before installation"
 sudo sed -i '/^[[:space:]]*libpwquality:/,$ s/^/#/' /usr/share/calamares/modules/users.conf
 sudo apt update && sudo apt install squashfs-tools
