66c9831808d19d305fe233136e7529bfc06e55c0
Zynomon aelius <92979020+zynomon@users.noreply.github.com>
2026-03-27 15:38:50 +0600

    Create e.sh

---
+++ e.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+
+isLive() {
+    rootfs=$(findmnt -n -o FSTYPE / 2>/dev/null)
+    [[ "$rootfs" == "squashfs" ]] && [[ -d "/usr/share/live" ]]
+}
+
+live_exec() {
+    if grep -q "26" /etc/os-release 2>/dev/null; then
+        bash ./LNS26.sh
+    fi
+}
+
+normal_exec() {
+    echo "no normal exec script for now"
+}
+
+echo "------------------------------------------------------------------${DATE}"
+echo "_Lets Begin___________________________________________________________________"
+echo ""
+
+if isLive; then
+    live_exec
+else
+    normal_exec
+fi
