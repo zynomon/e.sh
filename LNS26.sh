473e64d97c2daa3ebb3ef408a6a4d6f84ea381d2
Zynomon aelius <92979020+zynomon@users.noreply.github.com>
2026-03-28 11:39:14 +0600

    Update LNS26.sh

--- LNS26.sh
+++ LNS26.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo "run this before installation"
-sudo sed -i '/^[[:space:]]*libpwquality:/,$ s/^/#/' /usr/share/calamares/modules/users.conf
-sudo apt update && sudo apt install squashfs-tools
+
+sudo apt remove calamares-settings-error -y
+sudo apt update && sudo apt install squashfs-tools calamares-settings-error -y
+
+echo "completed."
