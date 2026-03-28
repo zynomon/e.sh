1eb0f53d0cb260840cdef496f103fec5ebd4ee1a
Zynomon aelius <92979020+zynomon@users.noreply.github.com>
2026-03-28 13:39:42 +0600

    Update NNS26.sh

--- NNS26.sh
+++ NNS26.sh
@@ -5,3 +5,6 @@ sudo apt-get update
 sudo apt remove -y --purge calamares calamares-settings-error partitionmanager squashfs-tools live-task-recommended live-task-localisation live-boot live-boot-doc live-config live-config-doc live-config-systemd live-tools 
 sudo apt install once -y && /bin/once ; sudo apt remove once
 sudo apt autoremove -y
+
+
+echo "thanks for using"
